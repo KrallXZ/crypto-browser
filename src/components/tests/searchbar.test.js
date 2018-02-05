bd0b71f6c339b8746cd27a8e49bc9b7ff4a1953c
Karol Syta <krallxz@gmail.com>
2018-02-05 19:58:21 +0100

    Reworked searchbar tests

--- src/components/tests/searchbar.test.js
+++ src/components/tests/searchbar.test.js
@@ -2,14 +2,18 @@ import React from 'react'
 import SearchBar from '../SearchBar'
 import renderer from 'react-test-renderer'
 
-test('SearchBar renders correctly', () => {
-  const component = renderer.create(<SearchBar />)
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+describe('SearchBar', () => {
+  it('renders correctly', () => {
+    const component = renderer.create(<SearchBar />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders as input', () => {
+    const component = renderer.create(<SearchBar />)
+    let tree = component.toJSON()
+    expect(tree.children[0].type).toBe('input')
+  })
 })
 
-test('SearchBar renders as input', () => {
-  const component = renderer.create(<SearchBar />)
-  let tree = component.toJSON()
-  expect(tree.children[0].type).toBe('input')
-})
+
